7d5ca2411fc48ffb1991c62f4b958a22079ac8f7
文龙 <wenlong@taobao.com>
2016-04-12 18:03:18 +0800

    InitalCommit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # gulp-oss-publish
-publish files to oss
+
+Publish files with oss
+
+## Install
+
+```shell
+npm install gulp-oss-publish
+```
+
+## Use
+
+```js
+import publish from 'gulp-oss-publish';
+
+gulp.task('publish', ['build'], () => {
+  return gulp
+    .src('dist/**/*', {
+      base: 'dist',
+      buffer: true
+    })
+    .pipe(publish({
+      prefix: 'a/puca-web',
+      genShortId: true,
+      oss: {
+        accessKeyId: 'xxx',
+        secretAccessKey: 'yyy',
+        endpoint: 'http://oss-cn-hangzhou.aliyuncs.com',
+        bucket: 'your-bucket-name'
+      },
+      headers: {
+        CacheControl: 'no-cache',
+        ServerSideEncryption: 'AES256'
+      }
+    }));
+});
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+var ALY = require('aliyun-sdk');
+var mime = require('mime');
+var path = require('path');
+var through2 = require('through2');
+var shortId = require('shortid');
+var urlJoin = require('url-join');
+var gutil = require('gulp-util');
+module.exports = publish;
+/**
+ * @params options.prefix root for oss
+ * @params {boolean} options.genShortId
+ * @params options.oss
+ * @params options.oss.accessKeyId
+ * @params options.oss.secretAccessKey
+ * @params options.oss.endpoint
+ */
+function publish(options) {
+  const oss = options.oss;
+  const prefix = options.prefix || '';
+  const uid = options.genShortId ? shortId.generate() : '';
+  const keyBase = urlJoin(prefix, uid);
+
+  var client = new ALY.OSS({
+    accessKeyId: oss.accessKeyId,
+    secretAccessKey: oss.secretAccessKey,
+    endpoint: oss.endpoint,
+    apiVersion: '2013-10-15'
+  });
+
+  var distFiles = [];
+
+  return through2.obj(function(file, enc, cb){
+    if (!file) return cb();
+    if (!file.isBuffer()) return cb();
+    this.push(file);
+    const key = urlJoin(keyBase, file.relative);
+
+    //console.log(key);
+
+    function onEnd(err) {
+      if (!err) {
+        distFiles.push(key);
+        cb();
+      } else {
+        cb(err)
+      }
+    }
+
+    onEnd();
+
+    // client.putObject(Object.assign({
+    //     ContentType: mime.lookup(file.relative),
+    //     CacheControl: 'no-cache',         // 参考: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.9
+    //     ContentEncoding: 'utf-8',         // 参考: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.11
+    //     ServerSideEncryption: 'AES256',
+    //     Expires: null                     // 参考: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.21
+    //   }, header || {}, {
+    //     Bucket: oss.bucket,
+    //     Key: urlJoin(prefix, uid, file.relative), // 注意, Key 的值不能以 / 开头, 否则会返回错误.
+    //     Body: file.contents
+    //   }), cb);
+
+  }, function(cb){
+    gutil.log('OSS publish finished,  %s files published', gutil.colors.cyan.underline(distFiles.length));
+    gutil.log('dist bucket: %s', gutil.colors.cyan(oss.bucket));
+    gutil.log('dist root: %s', gutil.colors.cyan(keyBase));
+    distFiles.forEach(function (key) {
+      gutil.log('file: %s', key);
+    });
+    cb();
+  });
+}
